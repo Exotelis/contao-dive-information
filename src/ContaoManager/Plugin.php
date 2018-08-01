b4f261a94fafcdb66c6308609d49922f1e259097
Sebastian Krah <exotelis@mailbox.org>
2018-08-01 09:12:46 +0200

    Initial commit
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of Exotelis/contao-DiveInformation
+ *
+ * Copyright (c) 2018-2018 Sebastian Krah
+ *
+ * @package   Exotelis/contao-DiveInformation
+ * @author    Sebastian Krah <exotelis@mailbox.org>
+ * @copyright 2018-2018 Sebastian Krah
+ * @license   https://github.com/Exotelis/contao-DiveInformation/blob/master/LICENSE LGPL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace Exotelis\ContaoDiveInformationBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Exotelis\ContaoDiveInformationBundle\ExotelisContaoDiveInformationBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @param ParserInterface $parser
+     *
+     * @return ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(ExotelisContaoDiveInformationBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                    ]
+                ),
+        ];
+    }
+}
