70dd075289d393a31c6f4b86647e2885634ab9b3
Sebastian Krah <exotelis@mailbox.org>
2018-08-01 09:15:38 +0200

    Initial commit
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

---
+++ src/ExotelisContaoDiveInformationBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of Exotelis/contao-DiveInformation
+ *
+ * Copyright (c) 2018-2018 Sebastian Krah
+ *
+ * @package   Exotelis/contao-DiveInformation
+ * @author    Sebastian Krah <exotelis@mailbox.org>
+ * @copyright 2018-2018 Sebastian Krah
+ * @license   https://github.com/Exotelis/contao-DiveInformation/blob/master/LICENSE LGPL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace Exotelis\ContaoDiveInformationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ExotelisContaoDiveInformationBundle extends Bundle
+{
+}
