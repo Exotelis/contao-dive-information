9410aba7ef33d4e7ac79be8c756560e31127818e
Sebastian Krah <exotelis@mailbox.org>
2018-08-01 12:37:24 +0200

    Initial commit
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This file is part of Exotelis/contao-DiveInformation
+ *
+ * Copyright (c) 2018-2018 Sebastian Krah
+ *
+ * @package   Exotelis/contao-DiveInformation
+ * @author    Sebastian Krah <exotelis@mailbox.org>
+ * @copyright 2018-2018 Sebastian Krah
+ * @license   https://github.com/Exotelis/contao-DiveInformation/blob/master/LICENSE LGPL-3.0
+ */
+
