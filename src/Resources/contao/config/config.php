e3c014e8c39e6d415c4caeea634080f864ff03fb
Sebastian Krah <exotelis@mailbox.org>
2018-08-01 17:11:24 +0200

    Update package name
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * This file is part of Exotelis/contao-DiveInformation
+ * This file is part of exotelis/contao-dive-information
  *
  * Copyright (c) 2018-2018 Sebastian Krah
  *
  * @package   Exotelis/contao-DiveInformation
  * @author    Sebastian Krah <exotelis@mailbox.org>
  * @copyright 2018-2018 Sebastian Krah
- * @license   https://github.com/Exotelis/contao-DiveInformation/blob/master/LICENSE LGPL-3.0
+ * @license   https://github.com/Exotelis/contao-dive-information/blob/master/LICENSE LGPL-3.0
  */
 
