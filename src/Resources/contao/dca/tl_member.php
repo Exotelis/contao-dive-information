9ba448c2e4290373e04528b95bb6c0f3c2f5d6f0
Sebastian Krah <exotelis@mailbox.org>
2018-08-06 10:05:37 +0200

    Add trainer as option
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -57,7 +57,7 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['nitrox'] = array
     'filter'                  => true,
     'flag'                    => 1,
     'inputType'               => 'select',
-    'options'                 => array('yes', 'no'),
+    'options'                 => array('yes', 'no', 'trainer'),
     'reference'               => &$GLOBALS['TL_LANG']['MSC'],
     'eval'                    => array('includeBlankOption'=>true, 'feEditable'=>false, 'feViewable'=>false, 'feGroup'=>'personal', 'tl_class'=>'w50'),
     'sql'                     => "varchar(5) NOT NULL default ''"
