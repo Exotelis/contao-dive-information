ab6d5373df19fb05bb5e5e949f0e37971f6aa07c
Sebastian Krah <exotelis@mailbox.org>
2018-08-09 11:04:38 +0200

    Extends class Contao\Newsletter
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

---
+++ src/Resources/contao/classes/Newsletter.php
@@ -0,0 +1,217 @@
+<?php
+
+/**
+ * This file is part of exotelis/contao-dive-information
+ *
+ * Copyright (c) 2018-2018 Sebastian Krah
+ *
+ * @package   exotelis/contao-dive-information
+ * @author    Sebastian Krah <exotelis@mailbox.org>
+ * @copyright 2018-2018 Sebastian Krah
+ * @license   https://github.com/Exotelis/contao-dive-information/blob/master/LICENSE LGPL-3.0
+ */
+
+declare(strict_types=1);
+
+namespace Exotelis;
+
+use Contao;
+
+/**
+ * Class Newsletter to extend Contaos Newsletter
+ *
+ * @author Sebastian Krah <https://github.com/Exotelis>
+ */
+class Newsletter extends Contao\Newsletter
+{
+    /**
+     * Update a particular member account
+     */
+    public function updateAccount()
+    {
+        $intUser = \Input::get('id');
+
+        // Front end call
+        if (TL_MODE == 'FE')
+        {
+            return;
+        }
+
+        // Return if there is no user (e.g. upon registration)
+        if (!$intUser)
+        {
+            return;
+        }
+
+        // Edit account
+        if (\Input::get('act') == 'edit')
+        {
+            $objUser = $this->Database->prepare("SELECT email, disable FROM tl_diver WHERE id=?")
+                ->limit(1)
+                ->execute($intUser);
+
+            if ($objUser->numRows)
+            {
+                $strEmail = \Input::post('email', true);
+
+                // E-mail address has changed
+                if (!empty($_POST) && $strEmail != '' && $strEmail != $objUser->email)
+                {
+                    $objCount = $this->Database->prepare("SELECT COUNT(*) AS count FROM tl_newsletter_recipients WHERE email=?")
+                        ->execute($strEmail);
+
+                    // Delete the old subscription if the new e-mail address exists (see #19)
+                    if ($objCount->count > 0)
+                    {
+                        $this->Database->prepare("DELETE FROM tl_newsletter_recipients WHERE email=?")
+                            ->execute($objUser->email);
+                    }
+                    else
+                    {
+                        $this->Database->prepare("UPDATE tl_newsletter_recipients SET email=? WHERE email=?")
+                            ->execute($strEmail, $objUser->email);
+                    }
+
+                    $objUser->email = $strEmail;
+                }
+
+                $objSubscriptions = $this->Database->prepare("SELECT pid FROM tl_newsletter_recipients WHERE email=?")
+                    ->execute($objUser->email);
+
+                if ($objSubscriptions->numRows)
+                {
+                    $strNewsletters = serialize($objSubscriptions->fetchEach('pid'));
+                }
+                else
+                {
+                    $strNewsletters = '';
+                }
+
+                $this->Database->prepare("UPDATE tl_diver SET newsletter=? WHERE id=?")
+                    ->execute($strNewsletters, $intUser);
+
+                // Check activation status
+                if (!empty($_POST) && \Input::post('disable') != $objUser->disable)
+                {
+                    $this->Database->prepare("UPDATE tl_newsletter_recipients SET active=? WHERE email=?")
+                        ->execute((\Input::post('disable') ? '' : 1), $objUser->email);
+
+                    $objUser->disable = \Input::post('disable');
+                }
+            }
+        }
+
+        // Delete account
+        elseif (\Input::get('act') == 'delete')
+        {
+            $objUser = $this->Database->prepare("SELECT email FROM tl_diver WHERE id=?")
+                ->limit(1)
+                ->execute($intUser);
+
+            if ($objUser->numRows)
+            {
+                $this->Database->prepare("DELETE FROM tl_newsletter_recipients WHERE email=?")
+                    ->execute($objUser->email);
+            }
+        }
+    }
+
+    /**
+     * Synchronize the newsletter subscriptions if the visibility is toggled
+     *
+     * @param boolean       $blnDisabled
+     * @param Contao\DataContainer $dc
+     *
+     * @return boolean
+     */
+    public function onToggleVisibility($blnDisabled, Contao\DataContainer $dc)
+    {
+        if (!$dc->id)
+        {
+            return $blnDisabled;
+        }
+
+        $objUser = $this->Database->prepare("SELECT email FROM tl_diver WHERE id=?")
+            ->limit(1)
+            ->execute($dc->id);
+
+        if ($objUser->numRows)
+        {
+            $this->Database->prepare("UPDATE tl_newsletter_recipients SET tstamp=?, active=? WHERE email=?")
+                ->execute(\time(), ($blnDisabled ? '' : '1'), $objUser->email);
+        }
+
+        return $blnDisabled;
+    }
+
+    /**
+     * Synchronize newsletter subscription of existing users
+     *
+     * @param mixed  $varValue
+     * @param object $objUser
+     * @param object $objModule
+     *
+     * @return mixed
+     */
+    public function synchronize($varValue, $objUser, $objModule=null)
+    {
+        // Return if there is no user (e.g. upon registration)
+        if ($objUser === null)
+        {
+            return $varValue;
+        }
+        $blnIsFrontend = true;
+        // If called from the back end, the second argument is a DataContainer object
+        if ($objUser instanceof Contao\DataContainer)
+        {
+            $objUser = $this->Database->prepare("SELECT * FROM tl_diver WHERE id=?")
+                ->limit(1)
+                ->execute($objUser->id);
+            if ($objUser->numRows < 1)
+            {
+                return $varValue;
+            }
+            $blnIsFrontend = false;
+        }
+        // Nothing has changed or e-mail address is empty
+        if ($varValue == $objUser->newsletter || $objUser->email == '')
+        {
+            return $varValue;
+        }
+        $time = \time();
+        $varValue = \StringUtil::deserialize($varValue, true);
+        // Get all channel IDs (thanks to Andreas Schempp)
+        if ($blnIsFrontend && $objModule instanceof Contao\Module)
+        {
+            $arrChannel = \StringUtil::deserialize($objModule->newsletters, true);
+        }
+        else
+        {
+            $arrChannel = $this->Database->query("SELECT id FROM tl_newsletter_channel")->fetchEach('id');
+        }
+        $arrDelete = \array_values(array_diff($arrChannel, $varValue));
+        // Delete existing recipients
+        if (!empty($arrDelete) && \is_array($arrDelete))
+        {
+            $this->Database->prepare("DELETE FROM tl_newsletter_recipients WHERE pid IN(" . \implode(',', array_map('\intval', $arrDelete)) . ") AND email=?")
+                ->execute($objUser->email);
+        }
+        // Add recipients
+        foreach ($varValue as $intId)
+        {
+            $intId = (int) $intId;
+            if ($intId < 1)
+            {
+                continue;
+            }
+            $objRecipient = $this->Database->prepare("SELECT COUNT(*) AS count FROM tl_newsletter_recipients WHERE pid=? AND email=?")
+                ->execute($intId, $objUser->email);
+            if ($objRecipient->count < 1)
+            {
+                $this->Database->prepare("INSERT INTO tl_newsletter_recipients SET pid=?, tstamp=$time, email=?, active=?, addedOn=?, ip=?")
+                    ->execute($intId, $objUser->email, ($objUser->disable ? '' : 1), ($blnIsFrontend ? $time : ''), ($blnIsFrontend ? \Environment::get('ip') : ''));
+            }
+        }
+        return \serialize($varValue);
+    }
+}
